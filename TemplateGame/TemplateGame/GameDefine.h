ad51e1cb6dbf5106585c7dd720f7f582b0d8dc61
AnhPmcl2015 <phananh.pmcl2015@gmail.com>
2019-02-20 18:49:01 +0700

    Thêm camera

--- TemplateGame/TemplateGame/GameDefine.h
+++ TemplateGame/TemplateGame/GameDefine.h
@@ -1,12 +1,17 @@
 #pragma once
 
-// ========= START: DEFAULT SetTING =========
+// ========= START: DEFAULT SETTING =========
 #define TRACE_LOG_PATH "Log\\trace.log";
 #define DEFAULT_SCREEN_MODE 0
 #define DEFAULT_IS_FULL_SCREEN false
 #define DEFAULT_FRAME_RATE 30
 #define DEFAULT_NAME_OF_GAME L"Game Developer for beginer"
-// ========= END: DEFAULT SetTING =========
+#define DEFAULT_CAMERA_WIDTH 1
+#define DEFAULT_CAMERA_HEIGHT 1
+#define DEFAULT_CAMERA_ANGLE 0.0F
+#define DEFAULT_CAMERA_START_POS_X 0.0f
+#define DEFAULT_CAMERA_END_POS_X 0.0f
+// ========= END: DEFAULT SETTING =========
 
 // ========= START: SCREEN DIMESION =========
 #define GAME_SCREEN_RESOLUTION_640_480_24   0
@@ -28,3 +33,10 @@ enum GAME_MODE {
 	GAME_MODE_END
 };
 // ========= END: GAME MODE =========
+
+// ========= START: CAMERA =========
+#define CAMERA_FOLLOW_POINT_LEFT_RATIO   2.0/4
+#define CAMERA_FOLLOW_POINT_RIGHT_RATIO  2.0/4
+#define CAMERA_FOLLOW_POINT_BOTTOM_RATIO 1.0/4
+#define CAMERA_FOLLOW_POINT_TOP_RATIO    3.0/4
+// ========= END: CAMERA =========
